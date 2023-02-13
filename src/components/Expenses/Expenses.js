ff2d127c1b151af2c877fef64b035f783b2790df
shkiimaa <shkiimaa@gmail.com>
2023-02-14 03:31:07 +0900

    필터링 기능 구현

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,25 +5,27 @@ import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
-  const [filteredYear, setfilteredYear] = useState('2020');
+  const [filteredYear, setfilteredYear] = useState('all');
 
   const fillterChangeHandler = (selectedYear) => {
     setfilteredYear(selectedYear);
   };
 
-  console.log(filteredYear);
+  const renderItems = props.items.filter((data) => {
+    return data.date.getFullYear().toString() === filteredYear;
+  });
 
   return (
     <>
       <Card className="expenses">
         <ExpensesFilter onChangeFilter={fillterChangeHandler} selectedYear={filteredYear} />
-        {props.items.map((data) => {
-          return <ExpenseItem key={data.id} title={data.title} amount={data.amount} date={data.date} filteredYear={filteredYear} />;
-        })}
-        {/* <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date} />
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date} />
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date} />
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date} /> */}
+        {filteredYear === 'all'
+          ? props.items.map((data) => {
+              return <ExpenseItem key={data.id} title={data.title} amount={data.amount} date={data.date} />;
+            })
+          : renderItems.map((data) => {
+              return <ExpenseItem key={data.id} title={data.title} amount={data.amount} date={data.date} />;
+            })}
       </Card>
     </>
   );
