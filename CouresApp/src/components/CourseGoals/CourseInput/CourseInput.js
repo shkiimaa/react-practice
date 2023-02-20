3f54bdccb3f2f5b35d77bd96e03c8f1994ade1cc
shkiimaa <shkiimaa@gmail.com>
2023-02-20 19:30:33 +0900

    스타일드 컴포넌트로 동적 css 적용하기

--- CouresApp/src/components/CourseGoals/CourseInput/CourseInput.js
+++ CouresApp/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
@@ -6,6 +7,8 @@ import './CourseInput.css';
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isValid, setIsValid] = useState(true);
+
+  const div = useRef();
 
   const goalInputChangeHandler = (event) => {
     if (event.target.value.trim().length > 0) {
@@ -23,22 +26,44 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
+  console.log(div.current);
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input
-          style={{
-            borderColor: !isValid ? 'red' : 'black',
-            background: !isValid ? 'salmon' : 'transparent',
-          }}
-          type="text"
-          onChange={goalInputChangeHandler}
-        />
-      </div>
+      <FormControl isValid={!isValid}>
+        <label ref={div}>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${(props) => (props.isValid ? 'red' : 'black')};
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${(props) => (props.isValid ? 'red' : '#ccc')};
+    background: ${(props) => (props.isValid ? '#ffd7d7' : 'transparent')};
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 export default CourseInput;
